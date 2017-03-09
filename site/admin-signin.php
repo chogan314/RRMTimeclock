ef25ed48ce6f938b3d28188d5876fefe3f509bb0
Cory Hogan <chogan314@users.noreply.github.com>
2017-03-08 18:16:57 -0600

    began admin security

---
+++ site/admin-signin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin signin</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="admin-signin.css">
+</head>
+<body>
+    <div id="content">
+        <div>
+            <form action="admin-validation.php" class="section column" id="admin-signin-form">
+                <h1>RRM Timesheet Admin Signin</h1>
+                <input type="text" name="username" class="input-item" id="username" placeholder="Username" autocomplete="off">
+                <input type="text" name="password" class="input-item" id="password" placeholder="Password" autocomplete="off">
+                <input type="submit" class="input-button" value="Submit">
+            </form>
+        </div>
+    </div>
+    <script src="jquery-3.1.1.min.js"></script>
+    <script>
+        var form = $('#admin-signin-form');
+        $(form).submit(function(event) {
+            event.preventDefault();
+            var formData = $(form).serialize();
+
+            $.ajax({
+                type: 'POST',
+                url: $(form).attr('action'),
+                data: formData
+            }).done(function(response) {
+                window.location = response;
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                //todo
+            });
+        });
+    </script>
+</body>
+</html>
---
+++ site/admin.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+if (!isset($_SESSION['admin-id'])) {
+    header("Location: admin-signin.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="admin.css">
+</head>
+<body>
+    <div id="content">
+        <div>
+            <div class="section column" id="links">
+                <a href="hours-report.php" class="input-item">View Report</a>
+                <a href="volunteer-cru.php" class="input-item">Edit Volunteers</a>
+                <a href="departments-view.php" class="input-item">Edit Departments</a>
+                <a href="assignments-view.php" class="input-item">Edit Assignments</a>
+                <a href="hours-cru.php" class="input-item">Edit Hours</a>
+                <a href="admin-signout.php" class="input-item">Logout</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
